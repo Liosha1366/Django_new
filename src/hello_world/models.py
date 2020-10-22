a2636bf7cdb529e677f33260a74d00734a09a582
Hroft <hroft1366@gmail.com>
2020-10-22 19:03:10 +0300

    error1

--- src/hello_world/models.py
+++ src/hello_world/models.py
@@ -6,15 +6,13 @@ class Aut_book(models.Model):
     author = models.CharField(
         'Авторы книг',
         max_length=20,
-        blank=False,
-        null=False
     )
     description = models.TextField(
         'Биография автора',
-        max_length=100,
         blank=True,
         null=True
     )
+
     def __str__(self):
         return self.author
 
