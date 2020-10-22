104247d5022c9b13cb6dde35dfa2ddc33ef55390
Hroft <hroft1366@gmail.com>
2020-10-22 18:39:41 +0300

    migrate01

--- src/hello_world/models.py
+++ src/hello_world/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Aut_book(models.Model): 
+    author = models.CharField(
+        'Авторы книг',
+        max_length=20,
+        blank=False,
+        null=False
+    )
+    description = models.TextField(
+        'Биография автора',
+        max_length=100,
+        blank=True,
+        null=True
+    )
+    def __str__(self):
+        return self.author
+
+
