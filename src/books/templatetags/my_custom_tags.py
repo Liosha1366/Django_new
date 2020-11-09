b61a806d6dcedf7f6081664900b9eed3b433fca2
Hroft <hroft1366@gmail.com>
2020-11-09 16:25:32 +0300

    not1

--- src/books/templatetags/my_custom_tags.py
+++ src/books/templatetags/my_custom_tags.py
@@ -5,5 +5,5 @@ register = template.Library()
 
 @register.simple_tag
 def store_title() -> str:
-    title = Book.objects.first().publish
+    title = Book.objects.first().aut_book
     return title
