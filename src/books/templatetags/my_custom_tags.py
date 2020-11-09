4bd5157e5f1def993b0e971bc83e774f91e73798
Hroft <hroft1366@gmail.com>
2020-11-09 16:47:21 +0300

    not my_custom_tags

--- src/books/templatetags/my_custom_tags.py
+++ src/books/templatetags/my_custom_tags.py
@@ -1,9 +1,9 @@
-from django import template
-from books.models import Book
-register = template.Library()
+# from django import template
+# from books.models import Book
+# register = template.Library()
 
 
-@register.simple_tag
-def store_title() -> str:
-    title = Book.objects.first().aut_book
-    return title
+# @register.simple_tag
+# def store_title() -> str:
+#     title = Book.objects.first().aut_book
+#     return title
